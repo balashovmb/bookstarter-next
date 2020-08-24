53ab1f927a103772e24d536eba7ba74c9f95591f
balashovmb <balashovmb@gmail.com>
2020-08-24 14:36:27 +0500

    Fix(modal root)

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
         <BookContainer {...this.props} />
         <Footer />
         <ToTheTopButton />
+        <div id="modal-root"></div>
       </>
     )
   }
